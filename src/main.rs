18890f7f18182ae484e5f83ee4c691ca1bd92498
Eddy Oyieko <67474838+mobley-trent@users.noreply.github.com>
2023-11-27 11:00:49 +0300

    Updated main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,6 @@ use std::fs;
 mod kata;
 
 fn main() {
-    let count = fs::read_dir("src/kata").unwrap().count();
+    let count = fs::read_dir(r"src\kata").unwrap().count();
     println!("The number of completed kata so far is {}", count - 1);
 }
