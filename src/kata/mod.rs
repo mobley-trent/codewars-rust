c562ec959e0cf0307b73a4af2f84e3d95b57d459
Eddy Oyieko <67474838+mobley-trent@users.noreply.github.com>
2024-01-08 18:22:05 +0300

    Updated mod.rs

--- src/kata/mod.rs
+++ src/kata/mod.rs
@@ -1,3 +1,4 @@
+// 7 Kyu
 pub mod odd_or_even;
 pub mod cc_mask;
 pub mod find_the_divisors;
@@ -5,7 +6,9 @@ pub mod reverse_words;
 pub mod gravity_flip;
 pub mod find_lowest_int;
 pub mod bubblesort_once;
-pub mod sum_array_singles;
 pub mod a_to_z;
 pub mod spoonerize_me;
 pub mod string_doubles;
+
+// 8 Kyu
+pub mod sum_array_singles;
